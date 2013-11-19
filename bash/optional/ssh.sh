8ce48925d2a970bfc3176954bd79f288b7e3bc79
Stephen Johnson <stephen@extrabux.com>
2013-11-19 06:50:53 -0800

    ssh autocompletion

--- bash/optional/ssh.sh
+++ bash/optional/ssh.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# Short-hand function for ssh connections
-#TODO: add some auto-completion for users and hosts
-function s {
-
-	if [ $# -eq 1 ]; then
-		local USER=$DEFAULT_SSH_USER
-		local HOST=$1
-	elif [ $# -eq 2 ]; then
-		local USER=$1
-		local HOST=$2
-	else 
-		echo "Usage: s <user> HOST"
-		return
-	fi
-	
-	# echo "HOST: $HOST, USER: $USER";
-	ssh $USER@$HOST
-}
 
 # SSH Agent
 if [ `ps aux | grep ssh-agent -c` -eq 1 ]; then
