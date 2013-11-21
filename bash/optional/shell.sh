8a2f9eff156f5b5f9db852147c3265797958e8d5
Stephen Johnson <stevej@ucla.edu>
2013-11-20 20:46:17 -0800

    better ssh completion

--- bash/optional/shell.sh
+++ bash/optional/shell.sh
@@ -17,30 +17,5 @@ fi
 # Ignore case when doing bash completion
 bind "set completion-ignore-case"
 
-# Autocompletion for SSH
-function _smj_devenv_ssh_known_hosts_completion {
-	cur=${COMP_WORDS[COMP_CWORD]}
-	if [[ "$cur" == *@* ]]; then
-		# Username entered
-		user=${cur%@*}
-		hostPrefix=${cur#*@}
-		host=`cat ~/.ssh/known_hosts | awk '{print $1}' | awk 'BEGIN { FS = "," } ; {print $1}' | tr "=" "\n" | egrep "^$hostPrefix"`
-		if [ -n "$host" ]; then 
-			use="$user@$host"
-		fi
-	else
-		# No username yet
-		case "$cur" in
-			a*) use="admin@" ;;
-			e*) use="ec2-user@" ;;
-			r*) use="root@" ;;
-			u*) use="ubuntu@" ;;
-		esac
-	fi
-	COMPREPLY=( $( compgen -W "$use" -- $cur ) )
-}
-complete -o default -o nospace -F _smj_devenv_ssh_known_hosts_completion ssh
-
-
 #TODO: add push/pop for CD
 
--- bash/optional/ssh.sh
+++ bash/optional/ssh.sh
@@ -6,3 +6,46 @@ if [ `ps aux | grep ssh-agent -c` -eq 1 ]; then
 	eval $(ssh-agent)
 	lso ~/.ssh/ | egrep "\-rw?\-\-\-\-" | awk '{print $3}' | xargs -L 1 ssh-add 
 fi
+
+# /etc/profile.d/complete-hosts.sh
+# Autocomplete Hostnames for SSH etc.
+# by Jean-Sebastien Morisset (http://surniaulula.com/)
+_complete_ssh () {
+	COMPREPLY=()
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+		
+	local user_host_list=""
+	local user_arr=('admin' 'ec2-user' 'root' 'ubuntu')
+
+	if [[ $cur == *@* ]]; then
+		local host_list=`{ 
+			for c in /etc/ssh_config /etc/ssh/ssh_config ~/.ssh/config
+				do [ -r $c ] && sed -n -e 's/^Host[[:space:]]//p' -e 's/^[[:space:]]*HostName[[:space:]]//p' $c
+			done
+			for k in /etc/ssh_known_hosts /etc/ssh/ssh_known_hosts ~/.ssh/known_hosts
+				do [ -r $k ] && egrep -v '^[#\[]' $k | cut -f 1 -d ' ' | sed -e 's/[,:].*//g'
+			done
+			sed -n -e 's/^[0-9][0-9\.]*//p' /etc/hosts; } | tr ' ' '\n' | egrep -v '\n' | tr '\n' ' ' | sed -E "s/[[:space:]]+/ /g" | tr ' ' '\n'`
+	
+		IFS=$'\n'
+		local host_array=()
+		host_arr=( $host_list )
+
+		for h in "${host_arr[@]}"; do 
+			for u in "${user_arr[@]}"; do 
+				user_host_list+="$u@$h"$'\n'
+			done
+		done
+	
+	else
+		for u in "${user_arr[@]}"; do 
+			user_host_list+="$u@"$'\n'
+		done
+	fi
+
+	COMPREPLY=( $(compgen -W "${user_host_list}" -- "${cur}"))
+	return 0
+}
+complete -o nospace -F _complete_ssh ssh
+
+
