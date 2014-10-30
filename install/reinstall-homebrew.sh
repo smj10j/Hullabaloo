3512923913e814d39f0beb58928dc3f2ae200d65
Stephen Johnson <stevej@ucla.edu>
2014-10-29 21:57:56 -0700

    add git-extras to install and update brew-list

--- install/reinstall-homebrew.sh
+++ install/reinstall-homebrew.sh
@@ -12,6 +12,7 @@ brew tap homebrew/php
 
 echo "Installing git..."
 brew reinstall git
+brew reinstall git-extras
 
 # http://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
 echo "Updating existing Mac coreutils..."
