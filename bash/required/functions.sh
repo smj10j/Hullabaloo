abe73741d978c0db81c48c8bd4bffd49cede123d
Stephen Johnson <stevej@ucla.edu>
2015-11-11 20:57:13 -0800

    Remove previous terminal profile before updating

--- bash/required/functions.sh
+++ bash/required/functions.sh
@@ -89,6 +89,7 @@ function _hullabaloo_reload {
 			echo ""
 			echo "Installing new Terminal profile and opening a new shell so the changes take effect immediately..."
 			echo ""
+			/usr/libexec/PlistBuddy -c "Delete ':Window Settings':hullabaloo" ~/Library/Preferences/com.apple.Terminal.plist  
 			open "$TERMINAL_PROFILE_PATH" && exit 0
 		else
 			$BASH_PATH -l && exit 0
