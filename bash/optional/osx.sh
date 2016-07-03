c88fe1631b40a02089f830afd764e6a27dec621d
Stephen Johnson <stevej@ucla.edu>
2016-07-02 17:49:55 -0700

    fix ulimit setting

--- bash/optional/osx.sh
+++ bash/optional/osx.sh
@@ -127,8 +127,8 @@ function _hullabaloo_clear_bluetooth_cache {
 # Give us more breathing room
 sudo launchctl limit maxfiles 65536 65536
 sudo launchctl limit maxproc 2048 2048
-ulimit -n 65536
-ulimit -u 2047
+sudo ulimit -n 65536
+sudo ulimit -u 2047
 
 
 
