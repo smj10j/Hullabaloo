03108a4d692f26511b5368061b3a4f3bcaf9c9ea
Stephen Johnson <stevej@ucla.edu>
2017-08-28 03:52:34 -0700

    Update manp path

--- bash/optional/shell.sh
+++ bash/optional/shell.sh
@@ -147,7 +147,7 @@ alias gwhois="whois -h geektools.com"
 
 # Use html instead of pdf for manp viewing
 function manp() {
-    MANP_DIR=~/Dropbox/Backups/Software/'Foxit\ MobilePDF'
+    MANP_DIR=~/Library/Mobile\ Documents/iCloud~com~pspdfkit~viewer/Documents
     mkdir -p "${MANP_DIR}"
     groffer --man --www --www-viewer="bash -c 'mv -f -t ${MANP_DIR} \${1} && open -a Google\ Chrome ${MANP_DIR}/\$(basename \${1})' bash" "$1" &
     echo "Generating HTML-formatted manpage in the background..."
